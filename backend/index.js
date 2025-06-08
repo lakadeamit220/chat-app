b49ad8e269b641385ef247f76a11e8aa882d84ec
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-08 07:07:26 +0530

    Added login functionality of user and login route.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import userRoute from "../backend/routes/userRoute.js";
 
 dotenv.config({});
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use("/api/v1/user", userRoute);
+
 
 app.listen(PORT, () => {
   connectDB();
