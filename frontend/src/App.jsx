9341c4259a1bf1e4ff1f3b787fea4a1b8a7a8833
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-12 10:07:05 +0530

    Added background image to app, Added SignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 function App() {
   return (
-    <>
+    <div
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1620498590128-b780a069fdc0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
       <h1 className="text-5xl text-center">Chat Application</h1>
-    </>
+    </div>
   );
 }
 
