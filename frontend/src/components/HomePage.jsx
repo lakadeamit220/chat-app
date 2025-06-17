cb31121997f5fd145c84cbdd3c4d3aebc767e593
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-17 17:27:00 +0530

    modified layout of HomePage.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,9 +4,11 @@ import Sidebar from "./Sidebar";
 export default function HomePage() {
   return (
     <>
-      <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <Sidebar />
-        <MessageContainer />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 border-4">
+          <Sidebar />
+          <MessageContainer />
+        </div>
       </div>
     </>
   );
