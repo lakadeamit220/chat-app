c1d1b36f9b4c4cc4f6ee2eab05cf127b79d66d15
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-24 12:07:29 +0530

    modified store

--- frontend/src/reduxStore/store.js
+++ frontend/src/reduxStore/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import messageReducer from "./messageSlice"
+import socketReducer from "./socketSlice"
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     message: messageReducer,
+    socket: socketReducer,
   }
 });
 
